2daef0b36254f5b8e059690213791d8854e13894
eleazar harold <haroldyewa@gmail.com>
2020-09-20 04:00:59 +0300

    dockerfile, makefile, main.go entrypoint file. Fixing DB connection error.

---
+++ api/go.mod
@@ -0,0 +1,12 @@
+module github.com/eleazar-harold/employee-service/api
+
+go 1.15
+
+require (
+	github.com/go-openapi/runtime v0.19.22
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/jinzhu/now v1.1.1 // indirect
+	github.com/joho/godotenv v1.3.0
+)
---
+++ api/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/eleazar-harold/employee-service/api/app"
+	"github.com/eleazar-harold/employee-service/api/config"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error getting env, %v", err)
+	}
+	config := config.GetConfig()
+
+	app := &app.App{}
+	app.Initialize(config)
+	app.Run(":" + os.Getenv("APPORT"))
+}
